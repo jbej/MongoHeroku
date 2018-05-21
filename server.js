e52a697a21d17e6628c8f119188315f581e3dedd
Justyna Bejgrowicz <justyna.bejgrowicz@interia.pl>
2018-05-21 15:48:34 +0300

    Change package.json or server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/nodeappdatabase', {
+mongoose.connect('mongodb://jbej:C1UkTOm4#@ds129540.mlab.com:29540/database-1e', {
     useMongoClient: true
 });
 
